72851fbc3e155e15d36f4d952bb7f11d640339fb
hiurydev <81237941+hiurydev@users.noreply.github.com>
2021-10-08 21:34:03 -0300

    First commit

---
+++ src/main/java/br/senac/devwebcommercial/DevwebCommercialApplication.java
@@ -0,0 +1,13 @@
+package br.senac.devwebcommercial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevwebCommercialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevwebCommercialApplication.class, args);
+	}
+
+}
